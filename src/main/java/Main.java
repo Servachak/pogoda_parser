2c98400d041a23ce07f584bf246b8d85b7b832b4
Servachak <servachak088@gmail.com>
2018-04-06 19:19:58 +0300

    06.04.2018

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,30 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-/**
- * Created by adavi on 05.04.2018.
- */
 public class Main {
-
-//    private static Document
 
     public static void main(String[] args) throws IOException {
 
         Document document = Jsoup.connect("http://meteopost.com/pogoda/kharkov/").get();
 
-        Elements elements = document.select("img");
+        Element tableWeather = document.select("table").get(2);
 
-        for (Element element : elements) {
-            System.out.println(element.attr("src"));
+
+        Elements firstRow = tableWeather.select("td");
+
+        int count = 0;
+        for (Element element : firstRow) {
+            if(count == 21){
+                System.out.println();
+                count = 0;
+            }
+            String date = element.select("center").text();
+            System.out.print(date + "     ");
+            count++;
         }
+//        String date = firstRow.select("center").text();
+
+
 
 
     }
